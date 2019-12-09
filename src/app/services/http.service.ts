6ea8133895b5d6ba83f18193dccac4802f224a07
Andres Bolivar <andresfebo187@gmail.com>
2019-12-09 00:25:56 -0500

    get por token corregido

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -18,12 +18,22 @@ export class HttpService {
     return this.http.post(url, data, options);
   }
 
-  get(serviceName: string, data: any){
-    let headers =  new HttpHeaders().set('Authorization', 'Bearer ' + data);    
-    const options = { header: headers, withCredentials: false };
-
+  get(serviceName: string, data: any, params: any = [], headers: any = []){
+    const token = window.localStorage.getItem("token");
+    let paramHeaders =  new HttpHeaders().set('Content-Type', 'application/json');
+    paramHeaders = paramHeaders.append('Authorization', 'Bearer ' + data);    
+    let paramQuery = new HttpParams();
+    headers.forEach(item => {
+      paramHeaders = paramHeaders.append(item.campo, item.valor);
+    });
+    if (params === null) {
+      params = [];
+    }
+    params.forEach(item => {
+      paramQuery = paramQuery.append(item.campo, item.valor);
+    });
     const url = environment.apiUrl + serviceName;
 
-    return this.http.get(url, options);
+    return this.http.get(url, { headers: paramHeaders, params: paramQuery });
   }
 }
